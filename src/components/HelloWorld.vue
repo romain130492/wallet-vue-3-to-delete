641f8ac2d97e357748339f33898dde1e66973386
romain130492 <rouiller.romain11@gmail.com>
2021-12-22 12:57:48 +0800

    ,

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,13 @@
 <template>
   <div>
 test hello v5
-  <button @click="action">Action</button>
-  {{log}}
-  {{log2}}
+<!--   <button @click="action">Action</button>
+  {{log}} -->
 
     <button @click="action2">Action2</button>
+{{log}} 
+      {{log2}}
+
   </div>
 </template>
 
@@ -17,72 +19,102 @@ import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
  import { ref, onMounted } from 'vue'
 
+import Matic from "maticjs"
+
 
 /* eslint-disable */
 export default {
   
   setup () {
+
+/*     const maticProvider = new WalletConnectProvider(
+  {
+    host: `https://rpc-mumbai.matic.today`,
+    callbacks: {
+      onConnect: console.log('connected'),
+      onDisconnect: console.log('disconnected!')
+    }
+  }
+)
+
+const ropstenProvider = new WalletConnectProvider({
+    host: `https://ropsten.infura.io/v3/70645f042c3a409599c60f96f6dd9fbc`,
+    callbacks: {
+      onConnect: console.log('connected'),
+      onDisconnect: console.log('disconnected')
+    }
+  })
+
+
+const maticWeb3 = new Web3(maticProvider)
+const ropstenWeb3 = new Web3(ropstenProvider) */
+
+
+
     let log = ref('test')
-        let log2 = []
+    let log2 = []
+    
     const action2 = async()=>{
       try {
 
         //  Create WalletConnect Provider
-const provider = new WalletConnectProvider({
-  infuraId:  "69b27f2252994134974da9602463680f" , // Required
-});
+        const provider = new WalletConnectProvider({
+          infuraId:  "69b27f2252994134974da9602463680f" , // Required
+        });
 
-//  Enable session (triggers QR Code modal)
-await provider.enable();
+        //  Enable session (triggers QR Code modal)
+        await provider.enable();
 
-//  Create Web3
-const web3 = new Web3(provider);
-console.log(web3,'the web3 test');
-log2.push('test????')
-log.value='rorororo'
- log2.push(Object.keys(web3.eth))  
-console.log(Object.keys(web3.eth) ,'Object.keys(web3)[0] '); 
-console.log(log2,'lgos2');
-console.log(web3.eth.requestAccounts,'web3.ethweb3.ethweb3.eth');
-const accounts = await web3.eth.requestAccounts()
-console.log(accounts,'the acocunts new 2');
+        //  Create Web3
+        const web3 = new Web3(provider);
+        console.log(web3,'the web3 test');
+        log2.push('test????')
+        log.value='rorororo'
+        log2.push(Object.keys(web3.eth))  
+        console.log(Object.keys(web3.eth) ,'Object.keys(web3)[0] '); 
+        console.log(log2,'lgos2');
+        console.log(web3.eth.requestAccounts,'web3.ethweb3.ethweb3.eth');
+        const accounts = await web3.eth.requestAccounts()
+        console.log(accounts,'the acocunts new 2');
 
-// Subscribe to accounts change
-provider.on("accountsChanged", (accounts) => {
-  console.log(accounts);
-});
+        // Subscribe to accounts change
+        provider.on("accountsChanged", (accounts) => {
+          console.log(accounts);
+        });
 
-// Subscribe to chainId change
-provider.on("chainChanged", (chainId) => {
-  console.log(chainId);
-});
+        // Subscribe to chainId change
+        provider.on("chainChanged", (chainId) => {
+          console.log(chainId);
+        });
 
-// Subscribe to session connection
-provider.on("connect", () => {
-  console.log("connect");
-});
+        // Subscribe to session connection
+        provider.on("connect", () => {
+          console.log("connect");
+        });
 
-// Subscribe to session disconnection
-provider.on("disconnect", (code, reason) => {
-  console.log(code, reason);
-});
-
-
-
-        
+        // Subscribe to session disconnection
+        provider.on("disconnect", (code, reason) => {
+          console.log(code, reason);
+        }); 
       } catch (error) {
         console.log(error,'error');
       }
-
     }
-       const action = async () =>{
+    
+     /*   const action = async () =>{
 
           try {
      
             console.log('action');
             const providerOptions = {
               walletconnect: {
-                  package: WalletConnectProvider, // required
+                  package: WalletConnectProvider, // required,
+                    options: {
+                      rpc: {
+                        0x89: "https://matic-mainnet.chainstacklabs.com",
+                      },
+                      network: "polygon-mainet",
+                    },
                   options: {
                     infuraId: "69b27f2252994134974da9602463680f" // required
                   }
@@ -95,8 +127,7 @@ provider.on("disconnect", (code, reason) => {
               cacheProvider: false, // optional
               providerOptions // required
             });
-              /*      web3Modal.cachedProvider = ""; // This is referring to your instance not the class
-          localStorage.removeItem("WEB3_CONNECT_CACHED_PROVIDER"); */
+ 
         console.log('here 1');
             const provider = await web3Modal.connect();
             console.log('here 2');
@@ -133,11 +164,12 @@ provider.on("disconnect", (code, reason) => {
             log.value = error?error.message:error;
       
           }
-      }
+      } */
 
     return {
-      action,
+/*       action, */
             action2,
+   
       log,
       log2
     }
